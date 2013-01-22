bd32cccbcc4a05a7ea33d1d98b87b364d2b0ffda
varunrau <varunrau@gmail.com>
2013-01-21 19:39:09 -0500

    added ssl support maybe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  before_filter :force_ssl
   # before_filter :check_if_valid
   # after_filter :update_time
 
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def force_ssl
+    if !request.ssl?
+      redirect_to :protocol => 'https'
+    end
   end
 
   def check_if_valid
